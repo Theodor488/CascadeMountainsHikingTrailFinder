184bc901ec7fd480576219974560ec88649d11f0
Theodor Suciu <theodorsuciu@outlook.com>
2024-01-04 09:20:31 -0800

    Added functionality to get coords for hike locations

--- CascadeMountainsHikingTrailFinder/Program.cs
+++ CascadeMountainsHikingTrailFinder/Program.cs
@@ -6,35 +6,48 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Microsoft.Extensions.Configuration;
 
-
-string lat = "48.075";
-string lon = "-121.706";
-string radius = "15";
-string limit = "200";
-
-// Get apiKey and apiHost from appsettings.json
-var builder = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-
-IConfigurationRoot configuration = builder.Build();
-
-var apiKey = configuration["RapidAPI:Key"];
-var apiHost = configuration["RapidAPI:Host"];
-string requestUriString = $"https://trailapi-trailapi.p.rapidapi.com/activity/?lat={lat}&limit={limit}&lon={lon}&radius={radius}&q-activities_activity_type_name_eq=hiking";
-
-
-var client = new HttpClient();
-var request = new HttpRequestMessage
+public class Program
 {
-    Method = HttpMethod.Get,
-    RequestUri = new Uri(requestUriString),
-    Headers =
-    {
-        { "X-RapidAPI-Key", apiKey },
-        { "X-RapidAPI-Host", apiHost },
-    },
-};
+    public string Lat;
+    public string Lon;
 
-TrailDataManager trailDataManager = new TrailDataManager();
-List<Hike> trailList = await trailDataManager.GetTrailsAsync(request);
+    public static async Task Main(string[] args)
+    {
+        // Get Coords
+        string[] coords = UserActions.GetCoords();
+        string lat = coords[0];
+        string lon = coords[1];
+
+        string radius = "50";
+        string limit = "200";
+
+        // Get apiKey and apiHost from appsettings.json
+        var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        IConfigurationRoot configuration = builder.Build();
+
+        var apiKey = configuration["RapidAPI:Key"];
+        var apiHost = configuration["RapidAPI:Host"];
+        string requestUriString = $"https://trailapi-trailapi.p.rapidapi.com/activity/?lat={lat}&limit={limit}&lon={lon}&radius={radius}&q-activities_activity_type_name_eq=hiking";
+
+
+        var client = new HttpClient();
+        var request = new HttpRequestMessage
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri(requestUriString),
+            Headers =
+                {
+                    { "X-RapidAPI-Key", apiKey },
+                    { "X-RapidAPI-Host", apiHost },
+                },
+        };
+
+        TrailDataManager trailDataManager = new TrailDataManager();
+        List<Hike> trailList = await trailDataManager.GetTrailsAsync(request);
+
+        
+    }
+}
---
+++ CascadeMountainsHikingTrailFinder/UserActions.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CascadeMountainsHikingTrailFinder
+{
+    public static class UserActions
+    {
+        public enum Locations
+        {
+            NorthernCascades,
+            CentralCascades,
+            SouthernCascades,
+            Unknown
+        }
+
+        public static string[] GetCoords()
+        {
+            Console.WriteLine("____Cascade Mountains Hiking Trail Finder____\n");
+            Console.WriteLine("Choose a location: 1.Northern Cascades, 2.Central Cascades, 3.Southern Cascades");
+            string locationChoice = Console.ReadLine();
+
+            string[] coords = ChooseLocation(locationChoice);
+            return coords;
+        }
+
+        public static string[] ChooseLocation(string locationChoice)
+        {
+            Locations location = Locations.Unknown;
+            bool askForLocation = true;
+
+            while (askForLocation)
+            {
+                if (locationChoice == "1")
+                {
+                    location = Locations.NorthernCascades;
+                    askForLocation = false;
+                }
+                else if (locationChoice == "2")
+                {
+                    location = Locations.CentralCascades;
+                    askForLocation = false;
+                }
+                else if (locationChoice == "3")
+                {
+                    location = Locations.SouthernCascades;
+                    askForLocation = false;
+                }
+                else
+                {
+                    Console.WriteLine("Unkown location. Choose 1 (North), 2 (Central) or 3 (South).");
+                }
+            }
+
+            string[] coords = AssignCoords(location);
+
+            return coords;
+        }
+
+        public static string[] AssignCoords(Locations location)
+        {
+            string[] coords = new string[2];
+
+            if (location == Locations.NorthernCascades)
+            {
+                coords[0] = "48.73"; // lat
+                coords[1] = "-121.11"; // lon
+            }
+            else if (location == Locations.CentralCascades)
+            {
+                coords[0] = "47.71"; // lat
+                coords[1] = "-121.31"; // lon
+            }
+            else if (location == Locations.SouthernCascades) 
+            {
+                coords[0] = "46.22"; // lat
+                coords[1] = "-121.62"; // lon 
+            }
+            else
+            {
+                coords[0] = "0"; // lat
+                coords[1] = "0"; // lon 
+            }
+
+            return coords;
+        }
+    }
+}
